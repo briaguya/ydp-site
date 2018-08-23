28f3e7ad58208b6deef393c0817632cf5d33a27e
Brian Smith <briaguya@gmail.com>
2018-08-23 14:27:35 -0500

    initit

---
+++ src/containers/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withRouteData } from "react-static";
+import convert from "htmr";
+//
+
+export default withRouteData(({ reactStatic }) => (
+  <div>
+    <section>{convert(reactStatic.contents)}</section>
+  </div>
+));
