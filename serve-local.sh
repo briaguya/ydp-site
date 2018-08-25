f69ba26a699383965fdcb0a6953c20674fbe151c
Brian Smith <briaguya@gmail.com>
2018-08-25 06:32:09 -0500

    force lockfile into it because who knows what's wrong with gitignore rn

---
+++ serve-local.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "starting..."
+
+set -o errexit
+
+TAG='janis:local'
+echo "building docker image..."
+docker build --tag "$TAG" .
+echo "running docker image..."
+HOST_IP=$(ifconfig en0 | awk '$1 == "inet" {print $2}')
+
+docker run \
+    --rm \
+    --name yourdadpresents \
+    --tty --interactive \
+    --publish 3000:80 \
+    --volume "$PWD/src:/app/src" \
+    --volume "$PWD/public:/app/public" \
+    --volume "$PWD/yarn.lock:/app/yarn.lock" \
+    --volume "$PWD/package.json:/app/package.json" \
+    --volume "$PWD/static.config.js:/app/static.config.js" \
+    --volume "$PWD/.babelrc:/app/.babelrc" \
+    "$TAG" "$@"
