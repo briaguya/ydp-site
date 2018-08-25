6d07de4644af26c58dfe6f2feddde776c7a1530f
Brian Smith <briaguya@gmail.com>
2018-08-25 07:17:00 -0500

    add a link for changing it

--- src/containers/WTF.js
+++ src/containers/WTF.js
@@ -10,13 +10,23 @@ const acronyms = [
   "Dope and Dope & Tight"
 ];
 
+const noways = [
+  "No way",
+  "But seriously...",
+  "That can't be real",
+  "What does it really mean though?",
+  "I don't believe you"
+];
+
 export default withRouteData(() => {
 
   const acronym = acronyms[Math.floor(Math.random()*acronyms.length)]
+  const noway = noways[Math.floor(Math.random()*noways.length)]
 
   return (
     <div>
       <div>{acronym}</div>
+      <Link to="/wtf">{noway}</Link>
     </div>
   );
 });
